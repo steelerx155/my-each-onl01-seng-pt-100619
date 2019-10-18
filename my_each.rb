a83022f4124513ca7020cb7a63550bfada7517ad
Randy Steele <steelerx155@gmail.com>
2019-10-18 18:15:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,5 @@
 def my_each (days)# put argument(s) here
-days = "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
   while i < days.length
   yield days[i] 
   i + i = 1
