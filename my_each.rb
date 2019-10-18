6d815e7f672adfa924275672665e217cb18052f4
Randy Steele <steelerx155@gmail.com>
2019-10-18 18:18:25 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,6 @@
-def my_each(days)# put argument(s) here
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+def my_each(days)  # put argument(s) here
+  if block_given?
+    i = 0
   while i < days.length
   yield(days[i])
   i = i + 1
