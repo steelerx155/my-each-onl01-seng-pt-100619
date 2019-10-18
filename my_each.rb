df12c15bab779b9bc70abbb554617e5e04a683ba
Randy Steele <steelerx155@gmail.com>
2019-10-18 18:19:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(days)  # put argument(s) here
+  if block_given?
+    i = 0
+    
+  while i < days.length
+  yield(days[i])
+  i = i + 1
+ end
+  days 
+ else
+end
 end
