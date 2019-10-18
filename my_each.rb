e476aa292f4537b9846e2dc7716b631113b7f72b
Randy Steele <steelerx155@gmail.com>
2019-10-18 17:22:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,7 @@
 def my_each (days)# put argument(s) here
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].each do
   while 
+ 
   
   
   
